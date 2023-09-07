839257b7f28c42000d522bb3e9871e5da08a93ef
jayasudha-1 <138743005+jayasudha-1@users.noreply.github.com>
2023-09-07 17:30:54 +0000

    Submission for Module 10-Jayasudha Kalamegam

---
+++ mern/hos09-client/src/components/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <a href="https://miniature-guacamole-v6v666gggjxvhp5xp-5050.app.github.dev/facebook" 
+    className="btn btn-primary">
+      Login with Facebook
+    </a>
+  );
+};
+
+export default Login;
